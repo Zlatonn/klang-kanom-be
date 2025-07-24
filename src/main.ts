2e1d2fabf1e2885adcf0724a43794fe4e7c6700f
zlatonn <tonkar2541@hotmail.com>
2025-07-24 16:16:17 +0700

    implement exception filter
    - create exceptions filter
    - apply to main

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { ExceptionsFilter } from './utils/filters/exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,8 @@ async function bootstrap() {
       forbidNonWhitelisted: false, // If any un expected properties are included, it will throw error
     }),
   );
+
+  app.useGlobalFilters(new ExceptionsFilter());
 
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
