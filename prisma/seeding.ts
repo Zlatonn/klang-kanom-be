249b526d72bd95e12cacd12b1467d6d0fdcb410e
zlatonn <tonkar2541@hotmail.com>
2025-07-25 15:39:23 +0700

    apply hash password for seeding

--- prisma/seeding.ts
+++ prisma/seeding.ts
@@ -1,15 +1,20 @@
 import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
 async function main() {
+  const saltRounds = Number(process.env.SALT_ROUND);
+  const adminPassword = await bcrypt.hash('password', saltRounds);
+  const userPassword = await bcrypt.hash('password', saltRounds);
+
   await prisma.user.createMany({
     skipDuplicates: true,
     data: [
       {
         id: 1,
         username: 'testadmin',
-        password: 'passw0rd',
+        password: adminPassword,
         firstName: 'test',
         lastName: 'admin',
         phoneNumber: '000-000-0000',
@@ -18,7 +23,7 @@ async function main() {
       {
         id: 2,
         username: 'testuser',
-        password: 'passw0rd',
+        password: userPassword,
         firstName: 'test',
         lastName: 'user',
         phoneNumber: '000-000-0001',
