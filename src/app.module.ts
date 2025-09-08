a9d4e2431926a1bca082ac6100168c6cf85563b5
zlatonn <tonkar2541@hotmail.com>
2025-09-08 09:06:17 +0700

    add serve static config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { PrismaModule } from './modules/prisma/prisma.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { APP_GUARD } from '@nestjs/core';
@@ -13,6 +15,10 @@ import { MenuModule } from './modules/menu/menu.module';
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: ['.env'],
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(process.cwd(), 'public/images'),
+      serveRoot: '/images',
     }),
     PrismaModule,
     AuthModule,
